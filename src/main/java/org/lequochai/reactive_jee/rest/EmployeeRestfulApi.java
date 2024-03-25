9d1693cf1009cb28b7f0826a9312e858300f24a6
Lê Quốc Hải <lequochai26@gmail.com>
2024-03-25 12:58:46 +0700

    Added getEmployee method for EmployeeRestfulApi.

--- src/main/java/org/lequochai/reactive_jee/rest/EmployeeRestfulApi.java
+++ src/main/java/org/lequochai/reactive_jee/rest/EmployeeRestfulApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -32,6 +33,35 @@ public class EmployeeRestfulApi {
         return response;
     }
 
+    @GET
+    @Path ("/{id}")
+    @Produces (MediaType.APPLICATION_JSON)
+    public Response<Employee> getEmployee(@PathParam("id") String idStr) {
+        Response<Employee> response = new Response<>();
+
+        int id;
+        try {
+            id = Integer.parseInt(idStr);
+        }
+        catch (Exception e) {
+            response.setSuccess(false);
+            response.setMessage(
+                "ID must be an integer!"
+            );
+            response.setCode("ID_INVALID");
+            return response;
+        }
+
+        response.setSuccess(true);
+        response.setResult(
+            EmployeeContainer
+                .getInstance()
+                .get(id)  
+        );
+
+        return response;
+    }
+
     @DELETE
     @Path ("/generateEmployees")
     @Produces (MediaType.APPLICATION_JSON)
@@ -42,13 +72,21 @@ public class EmployeeRestfulApi {
             EmployeeContainer
                 .getInstance()
                 .cancelGenerateEmployees();
-            
-            response.setSuccess(true);
         }
         catch (Exception e) {
             e.printStackTrace();
+
+            response.setSuccess(false);
+            response.setMessage(
+                "Failed while canceling generate employees!"
+            );
+            response.setCode("FAILED_CANCELING_GENERATE_EMPLOYEES");
+
+            return response;
         }
         
+        response.setSuccess(true);
+
         return response;
     }
 }
